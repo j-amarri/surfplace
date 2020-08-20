04588cf5cbb16ff8c6f5d6395179da6e7202a7f4
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-08-20 19:19:52 +0100

    Eliminate clutter files

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Surfplace</h1>
+      <h3>Project 3 Ironhack</h3>
+    </div>
+  );
+}
+
+export default App;
