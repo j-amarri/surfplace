a5c11a98e6077fea9cf69569bd3c366d9f75444d
Tyler Taboada <tjy5008@gmail.com>
2020-08-21 15:52:24 +0100

    Created board creation form

---
+++ client/src/components/BoardCreationForm.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+const BoardCreationForm = props => {
+  const handleFormSubmission = event => {
+    event.preventDefault();
+    props.onFormSubmission();
+  };
+
+  const handleDescriptionInputChange = event => {
+    const description = event.target.value;
+    props.onDescriptionChange(description);
+  };
+
+  const handlePhotoInputChange = event => {
+    const file = event.target.files[0];
+    props.onPhotoChange(file);
+  };
+
+  const handleNameInputChange = event => {
+    const name = event.target.value;
+    props.onNameChange(name);
+  };
+
+  const handleSizeInputChange = event => {
+    const size = event.target.value;
+    props.onSizeChange(size);
+  };
+
+  const handleLevelInputChange = event => {
+    const level = event.target.value;
+    props.onLevelChange(level);
+  };
+
+  const handlePriceInputChange = event => {
+    const price = event.target.value;
+    props.onPriceChange(price);
+  };
+
+  return (
+    <form onSubmit={handleFormSubmission}>
+      <label htmlFor="input-name">Name</label>
+      <input type="text" name="name" onChange={handleNameInputChange} />
+
+      <label htmlFor="name-input">Board Description</label>
+      <textarea
+        id="description-input"
+        placeholder="Describe the board here.."
+        name="description"
+        value={props.description}
+        onChange={handleDescriptionInputChange}
+      />
+
+      <label htmlFor="input-size">Board Size</label>
+      <input type="text" name="size" onChange={handleSizeInputChange} />
+
+      <label htmlFor="input-level">Skill Level</label>
+      <select type="level" name="level" onChange={handleLevelInputChange}>
+        <option value="beginner">Beginner</option>
+        <option value="intermediate">Intermediate</option>
+        <option value="advanced">Advanced</option>
+      </select>
+
+      <label htmlFor="input-price">Price</label>
+      <input type="text" name="price" onChange={handlePriceInputChange} />
+
+      {/* <label htmlFor="input-photo">Photo</label>
+      <input type="file" name="photo" onChange={handlePhotoInputChange} /> */}
+      <button>{props.isEdit ? 'Edit Board' : 'Add Board'}</button>
+    </form>
+  );
+};
+
+export default BoardCreationForm;
--- client/src/views/Boards/AddBoardView.jsx
+++ client/src/views/Boards/AddBoardView.jsx
@@ -1,26 +1,29 @@
-<h1>ADD BOARD VIEW</h1>;
+import React, { Component } from 'react';
 
-// import React, { Component } from 'react';
+class AddBoardView extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-// class AddBoardView extends Component {
-//   constructor() {
-//     super();
-//     this.state = {};
-//   }
+  //   handleBoardCreation = () => {};
 
-//   handleBoardCreation = () => {};
+  //   handleBoardDetailsChange = () => {};
 
-//   handleBoardDetailsChange = () => {};
+  //   handlePhotoChange = photo => {
+  //     this.setState({
+  //       photo
+  //     });
+  //   };
 
-//   handlePhotoChange = photo => {
-//     this.setState({
-//       photo
-//     });
-//   };
+  render() {
+    return (
+      <div>
+        <h1>BOARD CREATION VIEW</h1>
+        <BoardCreationForm />
+      </div>
+    );
+  }
+}
 
-//   render() {
-//     return <div></div>;
-//   }
-// }
-
-// export default AddBoardView;
+export default AddBoardView;
