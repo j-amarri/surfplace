4a5ee7be59776c75ecbc9a8a4219b14c3a01a23d
Tyler Taboada <tjy5008@gmail.com>
2020-08-24 18:20:38 +0100

    changes

--- client/src/views/Booking/CheckOutView.jsx
+++ client/src/views/Booking/CheckOutView.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 class CheckoutView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      board: null,
+      startDate: null,
+      endDate: null
+    };
+  }
+
   handleCheckout = () => {
     console.log('checking out');
   };
 
-  render() {
+  render(props) {
     return (
       <div className="checkout-container">
         <h1>Board checkout view</h1>
+        <p>{props.name}</p>
       </div>
     );
   }
