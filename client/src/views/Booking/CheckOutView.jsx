78ef3806f0f945b56d701aef03e45e4acd6f49e2
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-08-25 19:14:09 +0100

    Styling homepage

--- client/src/views/Booking/CheckOutView.jsx
+++ client/src/views/Booking/CheckOutView.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { loadMe } from './../../services/authentication';
 import { loadOrder } from './../../services/order';
+import moment from 'moment';
 
 class CheckoutView extends Component {
   constructor(props) {
@@ -15,7 +16,6 @@ class CheckoutView extends Component {
     const id = this.props.match.params.id;
     loadOrder(id)
       .then(data => {
-        console.log('component', data);
         this.setState({
           order: data,
           loaded: true
@@ -34,33 +34,21 @@ class CheckoutView extends Component {
     const order = this.state.order;
     return (
       <div className="checkout-container">
+        <div className="header-image">
+          <h1>Checkout</h1>
+        </div>
         {this.state.loaded && (
           <>
-            <h1>Rent your board</h1>
-            <h4>
-              <strong>Board name:</strong>
-              {order.product.name}
-            </h4>
-            <h4>
-              <strong>Board model:</strong>
-              {order.product.model}
-            </h4>
-            <h4>
-              <strong>Price per day:</strong>
-              {order.product.price}
-            </h4>
-            <h4>
-              <strong>Starting date:</strong>
-              {order.startDate}
-            </h4>
-            <h4>
-              <strong>End date:</strong>
-              {order.endDate}
-            </h4>
-            <h4>Days: {order.days}</h4>
-            <h4>Price: {order.total.amount}</h4>
-            <form onSubmit={this.handleCheckout} className="rent-link">
-              <button>Rent Board</button>
+            <h4>Your order details</h4>
+            <p>Board: {order.product.name}</p>
+            <p>Model: {order.product.model}</p>
+            <p>Price: {order.product.price}/day</p>
+            <p>
+              Dates: from {order.startDate} to {order.endDate}
+            </p>
+            <h3>Total price: {order.total.amount}</h3>
+            <form onSubmit={this.handleCheckout}>
+              <button className="checkout-button">Checkout</button>
             </form>
           </>
         )}
