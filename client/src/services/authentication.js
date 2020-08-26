a03c8a11eb945fae9f64c99c05f1db841c95d6ea
Tyler Taboada <tjy5008@gmail.com>
2020-08-26 20:40:57 +0100

    style changes

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://localhost:3020/authentication`,
+  baseURL: `${process.env.REACT_APP_BACKEND}/authentication`,
   withCredentials: true
 });
 
